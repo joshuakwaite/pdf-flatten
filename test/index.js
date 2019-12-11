95d4e1c4e91f495e8401c49da37bd90e25376132
Roy Hill-Percival <github@royhp.com>
2019-12-11 16:25:27 -0500

    Convert pdf2img to a class, fix options scope concurrency issues, and add a simple unit test

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var Flattener = require("../index.js");
 describe("Split and convert pdf into images", function() {
   it("Created a pdf file", function(done) {
 
-    const fileBuf = fs.readFileSync(__dirname + '/test.pdf', err => console.log(err))
+	const fileBuf = fs.readFileSync(__dirname + '/test.pdf', err => console.log(err))
     
     Flattener.flatten(fileBuf).then(res => {
       fs.writeFile("output.pdf", res, err => {
@@ -21,7 +21,27 @@ describe("Split and convert pdf into images", function() {
       fs.unlinkSync('output.pdf');
       if (err) console.log(err);
       done();
-    })
+	})
+	
+
+  });
+
+  it("Created a pdf file 2", function(done) {
+
+	const fileBuf2 = fs.readFileSync(__dirname + '/test.pdf', err => console.log(err))
+    
+	Flattener.flatten(fileBuf2).then(res => {
+		fs.writeFile("output2.pdf", res, err => {
+		  if (err) console.log(err);
+		  isFileExists("output2.pdf").should.to.be.true;
+		  fs.unlinkSync('output2.pdf');
+		});
+		done();
+	  }).catch(err => {
+		fs.unlinkSync('output2.pdf');
+		if (err) console.log(err);
+		done();
+	  })
 
   });
 
